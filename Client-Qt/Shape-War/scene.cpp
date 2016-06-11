8c478cb54f3638fa6c11c782e14726aaf2930daa
afg <afg984@gmail.com>
2016-06-11 15:50:28 +0800

    afg.format();

--- Client-Qt/Shape-War/scene.cpp
+++ Client-Qt/Shape-War/scene.cpp
@@ -1,14 +1,14 @@
 #include <scene.h>
 #include <QPainter>
 
-Scene::Scene(QWidget *parent) : QGraphicsView(parent){
-    this -> width = 800;
-    this -> height = 600;
-    this -> margin = 10;
+Scene::Scene(QWidget *parent) : QGraphicsView(parent) {
+    this->width = 800;
+    this->height = 600;
+    this->margin = 10;
     initView();
 }
 
-void Scene::initView(){
+void Scene::initView() {
     setRenderHint(QPainter::Antialiasing, true);
     setCacheMode(CacheBackground);
     setWindowTitle("Shape-War");
@@ -18,31 +18,33 @@ void Scene::initView(){
     setMaximumSize(width + margin, height + margin);
 
     QGraphicsScene* scene = new QGraphicsScene;
-    scene -> setSceneRect(5, 5, width, height);
+    scene->setSceneRect(5, 5, width, height);
     setScene(scene);
 }
 
-void Scene::drawBackground(QPainter *painter, const QRectF &rect){
+void Scene::drawBackground(QPainter *painter, const QRectF &rect) {
     // need to change position to global
     QColor backgroundColor(10, 10, 255, 30);
-    painter -> setBrush(backgroundColor);
-    painter -> setPen(backgroundColor);
-    for(int i=-5; i <= width + 20; i+=20)
-        painter -> drawLine(i, 5, i, height + 5);
+    painter->setBrush(backgroundColor);
+    painter->setPen(backgroundColor);
+    for (int i = -5; i <= width + 20; i += 20) {
+        painter->drawLine(i, 5, i, height + 5);
+    }
 
-    for(int i=-5; i <= height + 20; i+=20)
-        painter -> drawLine(5, i, width+5, i);
-
-}
-
-void Scene::startGame(){
+    for (int i = -5; i <= height + 20; i+=20) {
+        painter->drawLine(5, i, width + 5, i);
+    }
 
 }
 
-void Scene::initGame(){
+void Scene::startGame() {
 
 }
 
-void Scene::gameOver(){
+void Scene::initGame() {
+
+}
+
+void Scene::gameOver() {
 
 }
