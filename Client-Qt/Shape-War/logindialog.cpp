55c49f5863412212ad8570e45bb61e0e0e55d541
Zhe Yu <j2325138@gmail.com>
2016-06-15 01:30:00 +0800

    not tested yet

---
+++ Client-Qt/Shape-War/logindialog.cpp
@@ -0,0 +1,6 @@
+#include "logindialog.h"
+
+LoginDialog::LoginDialog()
+{
+
+}
---
+++ Client-Qt/Shape-War/logindialog.h
@@ -0,0 +1,11 @@
+#ifndef LOGINDIALOG_H
+#define LOGINDIALOG_H
+
+
+class LoginDialog
+{
+public:
+    LoginDialog();
+};
+
+#endif // LOGINDIALOG_H
