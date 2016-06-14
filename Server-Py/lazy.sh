0d3e74b96b5c19ed6066cb28ec78191deba99ec2
afg <afg984@gmail.com>
2016-06-14 15:49:37 +0800

    run server the lazy way

---
+++ Server-Py/lazy.sh
@@ -0,0 +1,11 @@
+set -e
+
+if [ ! -d venv ]; then
+    echo 'virtual environment not found, bootstrapping...'
+    virtualenv venv -p python3
+    source venv/bin/activate
+    pip install --upgrade pip
+    pip install -r requirements.txt
+fi
+
+venv/bin/python run.py
